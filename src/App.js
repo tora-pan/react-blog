8a9ceeba5cf2744cbaa205fd3b76ad1ba6fa6f98
tora_pan <travis.pandos@gmail.com>
2021-07-27 21:05:51 -0700

    finish settings page, add login page markup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Navbar from "./components/navbar/Navbar";
 import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
 import Settings from "./pages/settings/Settings";
 import SinglePostPage from "./pages/singlePost/SinglePostPage";
 import Write from "./pages/write/Write";
@@ -11,7 +12,8 @@ function App() {
       {/* <Home /> */}
       {/* <SinglePostPage /> */}
       {/* <Write /> */}
-      <Settings />
+      {/* <Settings /> */}
+      <Login />
     </div>
   );
 }
--- src/pages/settings/Settings.jsx
+++ src/pages/settings/Settings.jsx
@@ -1,18 +1,43 @@
-import Sidebar from '../../components/sidebar/Sidebar';
-import './settings.css';
+import { faUserCircle } from "@fortawesome/free-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Sidebar from "../../components/sidebar/Sidebar";
+import "./settings.css";
 
 const Settings = () => {
-    return (
-        <div className="settings">
-            <div className="settingsWrapper">
-                <div className="settingsTitle">
-                    <span className="settingsUpdateTitle">Update Your Account</span>
-                    <span className="settingsDeleteTitle">Delete Your Account</span>
-                </div>
-            </div>
-            <Sidebar />
+  return (
+    <div className="settings">
+      <div className="settingsWrapper">
+        <div className="settingsTitle">
+          <span className="settingsUpdateTitle">Update Your Account</span>
+          <span className="settingsDeleteTitle">Delete Your Account</span>
         </div>
-    )
-}
+        <form className="settingsForm">
+          <label>Profile Picture</label>
+          <div className="settingsProfilePicture">
+            <img
+              src="https://images.pexels.com/photos/1681010/pexels-photo-1681010.jpeg?cs=srgb&dl=pexels-creation-hill-1681010.jpg&fm=jpg"
+              alt="profile picture"
+            />
+            <label htmlFor="fileInput">
+              <FontAwesomeIcon
+                className="settingsProfilePictureIcon"
+                icon={faUserCircle}
+              />
+            </label>
+            <input type="file" id="fileInput" style={{ display: "none" }} />
+          </div>
+          <label>Username</label>
+          <input type="text" placeholder="User Name" />
+          <label>Email</label>
+          <input type="email" placeholder="Email" />
+          <label>Password</label>
+          <input type="password" placeholder="Password" />
+          <button className="settingsSubmit">Update</button>
+        </form>
+      </div>
+      <Sidebar />
+    </div>
+  );
+};
 
-export default Settings
+export default Settings;
